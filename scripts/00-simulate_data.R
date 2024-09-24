4d590d78d1a00c08a93a69092e5f5dc74b32c2c6
Maryam Ansari <mayyam.ansari@mail.utoronto.ca>
2024-09-24 14:27:13 -0400

    Ran the code, committing again

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -25,27 +25,27 @@ collision_data <- tibble(
   id = 1:number_of_rows,
   
   # Random date and time details
-  OCC_DATE = as.Date(runif(n = number_of_rows, 
+  date = as.Date(runif(n = number_of_rows, 
                            min = as.numeric(start_date), 
                            max = as.numeric(end_date)), 
                      origin = "1970-01-01"),
-  OCC_YEAR = sample(2014:2024, size = number_of_rows, replace = TRUE),
-  OCC_MONTH = sample(1:12, size = number_of_rows, replace = TRUE),
-  OCC_DOW = sample(c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"), 
+  year = sample(2014:2024, size = number_of_rows, replace = TRUE),
+  month = sample(1:12, size = number_of_rows, replace = TRUE),
+  week_day = sample(c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"), 
                    size = number_of_rows, replace = TRUE),
-  OCC_HOUR = sample(0:23, size = number_of_rows, replace = TRUE),
+  hour = sample(0:23, size = number_of_rows, replace = TRUE),
   
   # Collision details
-  FATALITIES = rpois(n = number_of_rows, lambda = 0.05),  # Simulating rare fatalities
-  INJURY_COLLISIONS = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.7, 0.3)),
-  PD_COLLISIONS = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.6, 0.4)),
+  fatalcount = rpois(n = number_of_rows, lambda = 0.05),  # Simulating rare fatalities
+  injurycount = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.7, 0.3)),
+  pdcollcount = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.6, 0.4)),
   
   # Involvement of different parties
-  AUTOMOBILE = sample(c(0, 1), size = number_of_rows, replace = TRUE),
-  MOTORCYCLE = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.95, 0.05)),
-  BICYCLE = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.9, 0.1)),
-  PEDESTRIAN = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.85, 0.15))
+  auto = sample(c(0, 1), size = number_of_rows, replace = TRUE),
+  motorcycle = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.95, 0.05)),
+  bicycle = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.9, 0.1)),
+  pedestrian = sample(c(0, 1), size = number_of_rows, replace = TRUE, prob = c(0.85, 0.15))
 )
 
 #### Save Data ####
-write_csv(collision_data, file = "data/raw_data/simulated_collision_data.csv")
+write_csv(collision_data, file = "data/raw_data/simulated_data.csv")
